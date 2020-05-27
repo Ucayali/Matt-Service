3e3af0e772bed1b3a7dffe0a228962a868888ffe
melvinioso <matt.mcgavern@gmail.com>
2020-05-26 22:13:26 -0500

    Successfully can connect to Cassandra and create a new keyspace

--- database/Cassandra.js
+++ database/Cassandra.js
@@ -1,13 +1,25 @@
 /* eslint-disable no-console */
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({
+const tempClient = new cassandra.Client({
   contactPoints: ['localhost'],
   localDataCenter: 'datacenter1',
 });
 
+const client = new cassandra.Client({
+  contactPoints: ['localhost'],
+  localDataCenter: 'datacenter1',
+  keySpace: 'item_details',
+});
 
-client.connect()
+tempClient.connect()
   .then(() => {
     console.log('Cassandra online!');
+  })
+  .then(() => {
+    tempClient.execute("CREATE KEYSPACE IF NOT EXISTS item_details WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+    console.log('item_details keyspace created!');
+  })
+  .then(() => {
+    client.connect((err) => (err ? console.log('Connection error!', err) : console.log('Connected to item_details keyspace!')));
   });
